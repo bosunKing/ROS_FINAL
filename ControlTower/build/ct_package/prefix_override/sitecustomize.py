03d5835f00eb0c3a95a6ca4fcbfd842fc011fb8f
bosun <mybosun99@gmail.com>
2024-06-17 15:20:34 +0900

    final_project

--- ControlTower/build/ct_package/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/bo/amr_ws/final/ros-repo-3/ControlTower/install/ct_package'
--- ControlTower/build/interface_package/ament_cmake_python/interface_package/interface_package/msg/__init__.py
+++
@@ -1,3 +0,0 @@
-from interface_package.msg._navfeedback import Navfeedback  # noqa: F401
-from interface_package.msg._navgoal import Navgoal  # noqa: F401
-from interface_package.msg._navresult import Navresult  # noqa: F401
